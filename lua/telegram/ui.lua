205ac3f1268882517bd64b135c66619ebb8e2e12
cyl <a1008611abcd@126.com>
2026-05-27 16:00:21 +0800

    Revert "fix: defer intro autocmd to avoid immediate CursorMoved clearing it"
    This reverts commit 2129e0da2367b82c35ba77dea4ad6ac45e015361.

--- lua/telegram/ui.lua
+++ lua/telegram/ui.lua
@@ -172,16 +172,14 @@ local function show_intro()
 		virt_text = { { "  Press @ for tools", "Comment" } },
 		virt_text_pos = "overlay",
 	})
-	vim.defer_fn(function()
-		vim.api.nvim_create_autocmd({ "InsertEnter", "BufWriteCmd", "CursorMoved" }, {
-			group = vim.api.nvim_create_augroup("TgIntroClear", { clear = true }),
-			buffer = state.buf,
-			once = true,
-			callback = function()
-				pcall(vim.api.nvim_buf_clear_namespace, state.buf, ns, 0, -1)
-			end,
-		})
-	end, 100)
+	vim.api.nvim_create_autocmd({ "InsertEnter", "BufWriteCmd", "CursorMoved" }, {
+		group = vim.api.nvim_create_augroup("TgIntroClear", { clear = true }),
+		buffer = state.buf,
+		once = true,
+		callback = function()
+			pcall(vim.api.nvim_buf_clear_namespace, state.buf, ns, 0, -1)
+		end,
+	})
 end
 
 function M.set_groups(groups)
