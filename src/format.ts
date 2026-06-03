6d5c0007569d6e2354a8111b475233ef5399aebb
cyl <a1008611abcd@126.com>
2026-06-03 11:50:45 +0800

    fix: revert download dedup that broke image loading

--- src/format.ts
+++ src/format.ts
@@ -49,7 +49,6 @@ export function extractText(content: { _: string; text?: { text: string }; capti
 }
 
 export class MessageFormatter {
-  private _downloadingFiles = new Set<number>();
   constructor(private resolver: Resolver, private invoke: (q: unknown) => Promise<unknown>) {}
 
   async format(msg: RawTdMessage | null, senderCache?: Map<string, SenderInfo>): Promise<FormattedMessage | null> {
@@ -100,11 +99,9 @@ export class MessageFormatter {
     }
     if (msg.content && msg.content._ && msg.content._ !== 'messageText') {
       this.invoke({ _: 'openMessageContent', chat_id: msg.chat_id, message_id: msg.id }).catch(() => {});
-      if (fileInfo && fileInfo.fileId > 0 && !this._downloadingFiles.has(fileInfo.fileId)) {
-        this._downloadingFiles.add(fileInfo.fileId);
+      if (fileInfo && fileInfo.fileId > 0) {
         this.invoke({ _: 'downloadFile', file_id: fileInfo.fileId, priority: 1 }).catch(() => {});
-        if (fileInfo.priorityFileId && fileInfo.priorityFileId !== fileInfo.fileId && !this._downloadingFiles.has(fileInfo.priorityFileId)) {
-          this._downloadingFiles.add(fileInfo.priorityFileId);
+        if (fileInfo.priorityFileId && fileInfo.priorityFileId !== fileInfo.fileId) {
           this.invoke({ _: 'downloadFile', file_id: fileInfo.priorityFileId, priority: 2 }).catch(() => {});
         }
       }
